6f3b4486ee652673b471f696cf01f55a2f507bc4
FacundoPeraltaa <facuperalta1234@gmail.com>
2025-05-28 14:39:13 -0300

    footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -2,18 +2,29 @@ import React from 'react';
 import styles from '../../styles/footer.module.scss';
 
 const Footer = () => {
-    return (
-        <footer className={styles.footer}>
-            <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="http://farmerin.com.ar/"
-                className={styles.link}
-            >
-                Farmerin División S.A. - © 2020
-            </a>
-        </footer>
-    );
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.content}>
+        <a
+          href="https://farmerin.com.ar/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+        >
+          Farmerin División S.A. © {new Date().getFullYear()}
+        </a>
+        <span className={styles.separator}>|</span>
+        <a
+          href="https://ultraidi.com.ar/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+        >
+          Ultra I+D
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
